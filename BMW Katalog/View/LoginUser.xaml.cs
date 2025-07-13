5109785b008f362539f0bc087a9437c4aa3a6121
knezzevic <161832003+knezzevic@users.noreply.github.com>
2025-07-13 16:46:20 +0200

    Napravljen Login
    Napravljen izgled Login forme, dodate biblioteke

---
+++ BMW Katalog/View/LoginUser.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BMW_Katalog.View
+{
+    /// <summary>
+    /// Interaction logic for LoginUser.xaml
+    /// </summary>
+    public partial class LoginUser : Window
+    {
+        public LoginUser()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed )
+            {
+                DragMove();
+            }
+            
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
